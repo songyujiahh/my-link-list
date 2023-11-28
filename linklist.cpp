6f32ed893ef7ca21b87fbdec89d353d3fd9ef171
songyujiahh <1530161642@qq.com>
2023-11-28 20:16:15 +0800

    提交

--- linklist.cpp
+++ linklist.cpp
@@ -51,6 +51,43 @@ bool insertnextnode(LNode *p,int e){
     p->next=s;
     return true;
 }
+bool insertpriorNode(LNode *p,int e){
+    if(p==NULL)
+        return false;
+    LNode *s=new LNode;
+    if(s==NULL){
+        return false;
+    }
+    s->next=p->next;
+    p->next=s;
+    s->data=p->data;
+    p->data=e;
+    return true;
+}
+bool listdelete(linklist &L,int i,int &e){
+    if(i<1){
+        return false;
+    }
+    LNode *p;
+    int j=0;
+    p=L;
+    while(p!=NULL && j<i-1){
+        p=p->next;
+        j++;
+    }
+    if(p==NULL)
+    {
+        return false;
+    }
+    if(p->next==NULL){
+        return false;
+    }
+    LNode *q=p->next;
+    e=q->data;
+    p->next=q->next;
+    free(q);
+    return true;
+}
 int main(){
     linklist L;
     initlist(L);
