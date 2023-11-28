4a773196f5917f250e7fd914a5d066c012eba44d
songyujiahh <1530161642@qq.com>
2023-11-28 19:29:45 +0800

    更新

--- linklist.cpp
+++ linklist.cpp
@@ -18,6 +18,39 @@ bool empty(linklist L){
     else 
         return false;
 }
+bool listinsert(linklist &L,int i,int e){
+    if(i<1){
+        return false;
+    }
+    LNode *p;
+    int j=0;
+    p=L;
+    while(p!=NULL && j<i-1){
+        p=p->next;
+        j++;
+    }
+    if(p==NULL){
+        return false;
+    }
+    LNode *s=new LNode;
+    s->data=e;
+    s->next=p->next;
+    p->next=s;
+    return true;
+}
+bool insertnextnode(LNode *p,int e){
+    if(p==NULL){
+        return false;
+    }
+    LNode *s=new LNode;
+    if(s==NULL){
+        return false;
+    } 
+    s->data=e;
+    s->next=p->next;
+    p->next=s;
+    return true;
+}
 int main(){
     linklist L;
     initlist(L);
